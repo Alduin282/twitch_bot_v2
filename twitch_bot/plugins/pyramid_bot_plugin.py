6a5d6d909e6a2844c2b25147f1018a0ed5f7098d
Alduin282 <pudge1515@mail.ru>
2026-01-07 16:25:22 +0500

    fix naming

--- twitch_bot/plugins/pyramid_bot_plugin.py
+++ twitch_bot/plugins/pyramid_bot_plugin.py
@@ -6,12 +6,12 @@ from twitchio import Message
 
 class PyramidBotPlugin(BotPlugin):
     COMMAND_NAME = "pyramid"
-    NO_PYRAMID_LEVEL_MESSAGE = ""
-    PYRAMID_LEVEL_IS_NOT_NUMBER_MESSAGE = "высота пирамиды не задана"
-    PYRAMID_LEVEL_LESS_MINIMUM_MESSAGE = "высота пирмаиды меньше минимальной"
-    PYRAMID_LEVEL_MORE_MAXIMUM_MESSAGE = "высота пирамиды больше максимальной"
-    MAXIMUM_PYRAMID_LEVEL = 10
-    MINIMUM_PYRAMID_LEVEL = 2
+    NO_PYRAMID_HEIGHT_MESSAGE = ""
+    PYRAMID_HEIGHT_IS_NOT_NUMBER_MESSAGE = "высота пирамиды не задана"
+    PYRAMID_HEIGHT_LESS_MINIMUM_MESSAGE = "высота пирмаиды меньше минимальной"
+    PYRAMID_HEIGHT_MORE_MAXIMUM_MESSAGE = "высота пирамиды больше максимальной"
+    MAXIMUM_PYRAMID_HEIGHT = 10
+    MINIMUM_PYRAMID_HEIGHT = 2
     DEFAULT_PYRAMID_SMILE = "LUL"
 
     def get_event_handlers(self) -> dict[EventType, EVENT_HANDLER]:
@@ -31,38 +31,38 @@ class PyramidBotPlugin(BotPlugin):
             await context_of_command.send(f"@{message.author.name} {problem_message}")
             return
 
-        pyramid_level = self._get_pyramid_level(text_of_command)
-        for pyramid_line in self._build_pyramid(pyramid_level):
+        pyramid_height = self._get_pyramid_height(text_of_command)
+        for pyramid_line in self._build_pyramid(pyramid_height):
             await context_of_command.send(pyramid_line)
 
     def _validate_pyramid_command(self, text_of_command: str) -> tuple[bool, str]:
         parts_of_command = text_of_command.split()
 
         if len(parts_of_command) < 2:
-            return False, self.NO_PYRAMID_LEVEL_MESSAGE
+            return False, self.NO_PYRAMID_HEIGHT_MESSAGE
 
         try:
-            pyramid_level = int(parts_of_command[1])
+            pyramid_height = int(parts_of_command[1])
         except ValueError:
-            return False, self.PYRAMID_LEVEL_IS_NOT_NUMBER_MESSAGE
+            return False, self.PYRAMID_HEIGHT_IS_NOT_NUMBER_MESSAGE
 
-        if pyramid_level < self.MINIMUM_PYRAMID_LEVEL:
-            return False, self.PYRAMID_LEVEL_LESS_MINIMUM_MESSAGE
+        if pyramid_height < self.MINIMUM_PYRAMID_HEIGHT:
+            return False, self.PYRAMID_HEIGHT_LESS_MINIMUM_MESSAGE
 
-        if pyramid_level > self.MAXIMUM_PYRAMID_LEVEL:
-            return False, self.PYRAMID_LEVEL_MORE_MAXIMUM_MESSAGE
+        if pyramid_height > self.MAXIMUM_PYRAMID_HEIGHT:
+            return False, self.PYRAMID_HEIGHT_MORE_MAXIMUM_MESSAGE
 
         return True, ""
 
-    def _build_pyramid(self, pyramid_level: int) -> list[str]:
+    def _build_pyramid(self, pyramid_height: int) -> list[str]:
         pyramid = []
 
-        for i in range(1, pyramid_level * 2):
-            current_level = i if i <= pyramid_level else pyramid_level * 2 - i
+        for i in range(1, pyramid_height * 2):
+            current_level = i if i <= pyramid_height else pyramid_height * 2 - i
             pyramid.append((f"{self.DEFAULT_PYRAMID_SMILE} " * current_level).strip())
 
         return pyramid
 
     @staticmethod
-    def _get_pyramid_level(text_of_command: str) -> int:
+    def _get_pyramid_height(text_of_command: str) -> int:
         return int(text_of_command.split()[1])
