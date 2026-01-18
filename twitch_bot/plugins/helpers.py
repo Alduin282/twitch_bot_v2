e4c029f52be1bf124e966f2c8207d9af06e2a988
Alduin282 <55664428+Alduin282@users.noreply.github.com>
2026-01-18 18:08:29 +0500

    add helpers tests (#11)
    * add helpers tests
    
    * add tests
    
    * fix
    
    * fixes

--- twitch_bot/plugins/helpers.py
+++ twitch_bot/plugins/helpers.py
@@ -21,8 +21,13 @@ class DurationRange:
     min_seconds: float = 0.0
     max_seconds: float = 0.0
 
-    # TODO валидация на больше 0
     def __post_init__(self):
+        if self.min_seconds < 0:
+            raise ValueError(f"min_seconds must be >= 0, got {self.min_seconds}")
+
+        if self.max_seconds < 0:
+            raise ValueError(f"max_seconds must be >= 0, got {self.max_seconds}")
+
         if self.min_seconds > self.max_seconds:
             raise ValueError(
                 f"Delay(min_seconds={self.min_seconds}, "
@@ -32,7 +37,7 @@ class DurationRange:
 
 
 async def sleep_in_range(duration_range: DurationRange) -> None:
-    if not duration_range.max_seconds > 0:
+    if duration_range.max_seconds <= 0:
         return
 
     await asyncio.sleep(
