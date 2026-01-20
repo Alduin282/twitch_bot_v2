110e4db113848a67d32779f2c293e5c17653cc8e
Alduin282 <pudge1515@mail.ru>
2026-01-20 14:21:52 +0500

    add ai tests

--- twitch_bot/plugins/ai_ask_bot_plugin.py
+++ twitch_bot/plugins/ai_ask_bot_plugin.py
@@ -7,6 +7,8 @@ from twitch_bot.twitch_bot import TwitchBot
 
 
 class AIAskPlugin(BotPlugin):
+    NO_QUESTION_ANSWER = "Нужно указать вопрос 😊 Пример: !ask как дела?"
+
     def __init__(self, ai_service: AIOllamaService, cooldown_seconds: int = 30) -> None:
         self._ai = ai_service
         self._cooldown_seconds = cooldown_seconds
@@ -29,7 +31,7 @@ class AIAskPlugin(BotPlugin):
 
         question = content[4:].strip()
         if not question:
-            await channel.send("Нужно указать вопрос 😊 Пример: !ask как дела?")
+            await channel.send(self.NO_QUESTION_ANSWER)
             return
 
         answer = await self._ai.answer(question)
