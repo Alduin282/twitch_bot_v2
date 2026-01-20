50a58741221204c3a0c8f96b94975db05ccbc373
Alduin282 <55664428+Alduin282@users.noreply.github.com>
2026-01-20 17:50:26 +0500

    add ai persona tests (#17)
    * add ai persona tests
    
    * fixes

---
+++ twitch_bot/plugins/tests/test_ai_persona_bot_plugin/test_get_event_handlers.py
@@ -0,0 +1,25 @@
+import unittest
+from unittest.mock import AsyncMock
+
+from twitch_bot.definitions import EventType
+from twitch_bot.plugins.ai_persona_plugin import AIPersonaPlugin
+
+
+class TestAIPersonaBotPluginGetEventHandlers(unittest.TestCase):
+
+    def setUp(self) -> None:
+        ai = AsyncMock()
+        self.plugin: AIPersonaPlugin = AIPersonaPlugin(ai)
+
+    def test__result__contains_message_event(self) -> None:
+        event_handlers = self.plugin.get_event_handlers()
+
+        self.assertIn(EventType.MESSAGE, event_handlers)
+
+    def test__on_message_event_handler__callable_and_has_right_name(self) -> None:
+        event_handlers = self.plugin.get_event_handlers()
+
+        self.assertTrue(callable(event_handlers[EventType.MESSAGE]))
+        self.assertEqual(
+            event_handlers[EventType.MESSAGE].__name__, self.plugin._on_message.__name__
+        )
---
+++ twitch_bot/plugins/tests/test_ai_persona_bot_plugin/test_on_message.py
@@ -0,0 +1,107 @@
+from unittest import IsolatedAsyncioTestCase
+from unittest.mock import AsyncMock, MagicMock
+
+from twitch_bot.plugins.ai_persona_plugin import AIPersonaPlugin
+
+
+class TestAIPersonaOnMessage(IsolatedAsyncioTestCase):
+
+    async def test__persona_command__sends_current_persona(self) -> None:
+        persona = "test persona"
+        ai_service = self._get_ai_service_mock(persona)
+
+        plugin = AIPersonaPlugin(ai_service)
+        command = plugin.GET_PERSONA_COMMAND
+        message_with_persona_command = self._get_message_mock(content=command)
+
+        await plugin._on_message(MagicMock(), message_with_persona_command)
+
+        ai_service.get_persona.assert_called_once()
+        message_with_persona_command.channel.send.assert_awaited_once_with(persona)
+
+    async def test__persona_random_not_mod__do_nothing(self) -> None:
+        ai_service = self._get_ai_service_mock()
+        plugin = AIPersonaPlugin(ai_service)
+
+        command = plugin.RANDOM_PERSONA_COMMAND
+        message_with_command = self._get_message_mock(command, is_mod=False)
+
+        await plugin._on_message(MagicMock(), message_with_command)
+
+        ai_service.set_random_persona.assert_not_called()
+        message_with_command.channel.send.assert_not_awaited()
+
+    async def test__persona_random_mod__updates_persona(self) -> None:
+        ai_service = self._get_ai_service_mock()
+        plugin = AIPersonaPlugin(ai_service)
+
+        command = plugin.RANDOM_PERSONA_COMMAND
+        message_with_command = self._get_message_mock(command, is_mod=True)
+
+        await plugin._on_message(MagicMock(), message_with_command)
+
+        ai_service.set_random_persona.assert_awaited_once()
+        message_with_command.channel.send.assert_awaited_once()
+
+    async def test__persona_reset_not_mod__do_nothing(self) -> None:
+        ai_service = self._get_ai_service_mock()
+        plugin = AIPersonaPlugin(ai_service)
+
+        command = plugin.RESET_PERSONA_COMMAND
+        message_with_command = self._get_message_mock(command, is_mod=False)
+
+        await plugin._on_message(MagicMock(), message_with_command)
+
+        ai_service.set_random_persona.assert_not_called()
+        message_with_command.channel.send.assert_not_awaited()
+
+    async def test__persona_reset_mod__resets_persona(self) -> None:
+        ai_service = self._get_ai_service_mock()
+        plugin = AIPersonaPlugin(ai_service)
+
+        command = plugin.RESET_PERSONA_COMMAND
+        message_with_command = self._get_message_mock(command, is_mod=True)
+
+        await plugin._on_message(MagicMock(), message_with_command)
+
+        ai_service.reset_persona.assert_called_once()
+        message_with_command.channel.send.assert_awaited_once()
+
+    async def test__unknown_command__do_nothing(self) -> None:
+        ai_service = self._get_ai_service_mock()
+        plugin = AIPersonaPlugin(ai_service)
+
+        message_with_unknown_command = self._get_message_mock(
+            f"{plugin.PREFIX} unknown_command"
+        )
+
+        await plugin._on_message(MagicMock(), message_with_unknown_command)
+
+        message_with_unknown_command.channel.send.assert_not_awaited()
+
+    @staticmethod
+    def _get_ai_service_mock(persona: str = "test_persona") -> MagicMock:
+        ai_service = MagicMock()
+        ai_service.get_persona.return_value = persona
+        ai_service.set_random_persona = AsyncMock()
+        return ai_service
+
+    @staticmethod
+    def _get_channel_mock():
+        channel = AsyncMock()
+        return channel
+
+    def _get_message_mock(
+        self,
+        content: str,
+        is_mod: bool = False,
+    ):
+        message = MagicMock()
+        message.content = content
+        message.channel = self._get_channel_mock()
+
+        author = MagicMock()
+        author.is_mod = is_mod
+        message.author = author
+
+        return message
