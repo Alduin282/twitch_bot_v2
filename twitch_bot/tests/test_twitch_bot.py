18f301132b14060c6841fb1b26d850785a7f34ca
Alduin282 <pudge1515@mail.ru>
2026-01-21 18:32:40 +0500

    add tests without mocks

--- twitch_bot/tests/test_twitch_bot.py
+++ twitch_bot/tests/test_twitch_bot.py
@@ -1,45 +1,61 @@
 from unittest import IsolatedAsyncioTestCase
-from unittest.mock import AsyncMock, MagicMock
-
+from unittest.mock import MagicMock
+from twitch_bot.event_dispatchers.event_dispatcher import EventDispatcher
+from twitch_bot.plugins.bot_plugin import BotPlugin
 from twitch_bot.twitch_bot import TwitchBot
 from twitch_bot.definitions import EventType
 
 
-class TestTwitchBotEventReady(IsolatedAsyncioTestCase):
+class TestPlugin(BotPlugin):
+    def __init__(self):
+        self.ready_called = False
+        self.message_called_with = []
 
-    async def test__event_ready__dispatches_ready_event(self) -> None:
-        dispatcher = AsyncMock()
+    def get_event_handlers(self):
+        return {
+            EventType.READY: self.on_ready,
+            EventType.MESSAGE: self.on_message,
+        }
+
+    async def on_ready(self, bot):
+        self.ready_called = True
+
+    async def on_message(self, bot, message):
+        self.message_called_with.append(message)
+
+
+class TestIntegrationTwitchBot(IsolatedAsyncioTestCase):
+
+    async def test__ready_event_dispatches_to_plugin__no_errors(self):
+        plugin = TestPlugin()
+
+        dispatcher = EventDispatcher([plugin])
 
         bot = TwitchBot(
-            token="test_token",
+            token="fake_token",
             channels_to_connect=["channels_to_connect"],
-            twitch_secret_key="twitch_secret_key",
+            twitch_secret_key="secret",
             event_dispatcher=dispatcher,
         )
 
         await bot.event_ready()
 
-        dispatcher.dispatch.assert_awaited_once_with(
-            EventType.READY,
-            bot,
-        )
+        assert plugin.ready_called is True
 
-    async def test__event_message__dispatches_message_event(self) -> None:
-        dispatcher = AsyncMock()
+    async def test__on_message_event_dispatches_to_plugin__no_errors(self):
+        plugin = TestPlugin()
+        dispatcher = EventDispatcher([plugin])
 
         bot = TwitchBot(
-            token="test_token",
+            token="fake_token",
             channels_to_connect=["channels_to_connect"],
-            twitch_secret_key="twitch_secret_key",
+            twitch_secret_key="secret",
             event_dispatcher=dispatcher,
         )
 
         message = MagicMock()
+        message.content = "message_content"
 
         await bot.event_message(message)
 
-        dispatcher.dispatch.assert_awaited_once_with(
-            EventType.MESSAGE,
-            bot,
-            message,
-        )
+        assert plugin.message_called_with == [message]
